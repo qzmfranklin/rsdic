b9f75e867b167a594f3355772d5c5096e61b9608
Zhongming Qu <qzmfranklin@gmail.com>
2015-09-10 16:57:00 -0700

    Mv src/gtest_builder.cpp to builder/

---
+++ src/dict/WordTree.h
@@ -0,0 +1,10 @@
+#ifndef __dict__WordTree__h__
+#define __dict__WordTree__h__
+
+class WordTree {
+public:
+    WordTree(const void *ptr, const size_t len);
+private:
+};
+
+#endif /* end of include guard  */
